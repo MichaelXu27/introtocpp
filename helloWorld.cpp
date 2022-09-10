0ee49c2d0dc48407443b552cfe28a883a02d93dd
Bombim <75459900+Bombim@users.noreply.github.com>
2022-09-10 11:32:49 -0400

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,10 @@
+#include<iostream> //this line imports the input and output library, "io" means input output
+
+using namespace std;
+
+int main()
+{
+	cout<< "Hello, World!"<< endl;
+	
+	return 0;
+}
